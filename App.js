5149b8cbbc0504ca0934ec46c18eab52ed197b47
Sebastián VaRom <sneeyder.sebas@gmail.com>
2022-04-08 21:57:23 -0500

    Structure and something more

--- App.js
+++ App.js
@@ -3,11 +3,12 @@
 import * as React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NativeBaseProvider } from "native-base";
-import HomeScreen from "./src/Screens/Home";
+import HomeScreen from "./src/Screens/Student/Home";
 import LoginScreen from "./src/Screens/Login";
-import ProfileScreen from "./src/Screens/Profile";
-import CalendarScreen from "./src/Screens/Calendar";
-import HistoryScreen from "./src/Screens/History";
+import ProfileScreen from "./src/Screens/Student/Profile";
+import CalendarScreen from "./src/Screens/Student/Calendar";
+import HistoryScreen from "./src/Screens/Student/History";
+import HomeTeacherScreen from "./src/Screens/Teacher/Home";
 import { NavigationContainer } from "@react-navigation/native";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
@@ -15,8 +16,6 @@ import { createMaterialBottomTabNavigator } from "@react-navigation/material-bot
 const Tab = createMaterialBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 function MyTabs({ navigation, route }) {
-  const name = route.params;
-  const [color, setColor] = React.useState();
   return (
     <Tab.Navigator
       initialRouteName="Home"
@@ -25,44 +24,52 @@ function MyTabs({ navigation, route }) {
     >
       <Tab.Screen
         name="Home"
-        component={() => <HomeScreen name={route.params.name} />}
         options={{
           tabBarColor: "#041C32",
           tabBarIcon: ({ color }) => (
             <MaterialCommunityIcons name="home" color={color} size={26} />
           ),
         }}
-      />
+      >
+        {route.params.data.type === "Student"
+          ? (props) => <HomeScreen {...props} data={route.params.data} />
+          : route.params.data.type === "Teacher"
+          ? (props) => <HomeTeacherScreen {...props} data={route.params.data} />
+          : window.alert("Datos erroneos", window.location.reload())}
+      </Tab.Screen>
       <Tab.Screen
         name="Agenda"
-        component={() => <CalendarScreen name={route.params.name} />}
         options={{
           tabBarColor: "#04293A",
           tabBarIcon: ({ color }) => (
             <MaterialCommunityIcons name="calendar" color={color} size={26} />
           ),
         }}
-      />
+      >
+        {(props) => <CalendarScreen {...props} name={route.params.name} />}
+      </Tab.Screen>
       <Tab.Screen
         name="Historial"
-        component={() => <HistoryScreen name={route.params.name} />}
         options={{
           tabBarColor: "#064663",
           tabBarIcon: ({ color }) => (
             <MaterialCommunityIcons name="bookshelf" color={color} size={26} />
           ),
         }}
-      />
+      >
+        {(props) => <HistoryScreen {...props} name={route.params.name} />}
+      </Tab.Screen>
       <Tab.Screen
         name="Profile"
-        component={() => <ProfileScreen name={route.params.name} />}
         options={{
           tabBarColor: "#ECB365",
           tabBarIcon: ({ color }) => (
             <MaterialCommunityIcons name="account" color={color} size={26} />
           ),
         }}
-      />
+      >
+        {(props) => <ProfileScreen {...props} name={route.params.name} />}
+      </Tab.Screen>
     </Tab.Navigator>
   );
 }
