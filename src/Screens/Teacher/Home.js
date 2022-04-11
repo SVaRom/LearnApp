94e2537dea9843242c09aaf10020f8cb0abc0e04
Sebastián Rodríguez Martínez <srm2000@outlook.com>
2022-04-11 07:49:39 -0500

    Update Calendar/Agenda

--- src/Screens/Teacher/Home.js
+++ src/Screens/Teacher/Home.js
@@ -1,18 +1,11 @@
 import React from "react";
-import { TextInput, Button, View, Text } from "react-native";
+import { View, Text } from "react-native";
+import { Agenda } from "react-native-calendars";
 const Home = ({ navigation, data }) => {
   const [text, setText] = React.useState("");
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        display: "flex",
-        margin: 10,
-      }}
-    >
-      <Text>Aquí es home del teacher {data.number}</Text>
+    <View style={{ flex: 1 }}>
+      <Agenda />
     </View>
   );
 };
