efa77fd74196ab2e4d135a5632b2356fb7f5d512
Sebastián Rodríguez Martínez <30491953+SLegendary@users.noreply.github.com>
2022-04-20 19:36:12 -0500

    Update Home.js

--- src/Screens/Teacher/Home.js
+++ src/Screens/Teacher/Home.js
@@ -1,27 +1,126 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { TouchableOpacity } from "react-native";
+import {
+  NativeBaseProvider,
+  View,
+  Fab,
+  Box,
+  Modal,
+  FormControl,
+  Input,
+  Button,
+} from "native-base";
 import { Agenda } from "react-native-calendars";
-const Home = ({ navigation, data }) => {
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+
+const timeToString = (time) => {
+  const date = new Date(time);
+  return date.toISOString().split("T")[0];
+};
+
+const Home = () => {
+  const [items, setItems] = useState({});
+
+  const loadItems = (day) => {
+    setTimeout(() => {
+      for (let i = -15; i < 85; i++) {
+        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+        const strTime = timeToString(time);
+        if (!items[strTime]) {
+          items[strTime] = [];
+          const numItems = Math.floor(Math.random() * 3 + 1);
+          for (let j = 0; j < numItems; j++) {
+            items[strTime].push({
+              name: "Item for " + strTime + " #" + j,
+              height: Math.max(50, Math.floor(Math.random() * 150)),
+            });
+          }
+        }
+      }
+      const newItems = {};
+      Object.keys(items).forEach((key) => {
+        newItems[key] = items[key];
+      });
+      setItems(newItems);
+    }, 1000);
+  };
+
+  const renderItem = (item) => {
+    return (
+      <TouchableOpacity style={{ marginRight: 10, marginTop: 17 }}>
+        <Box
+          rounded="xl"
+          w="100%"
+          m={1}
+          bg="coolGray.400"
+          p="6"
+          shadow={2}
+          _text={{
+            fontSize: "md",
+            fontWeight: "bold",
+            color: "white",
+          }}
+        >
+          {item.name}
+        </Box>
+      </TouchableOpacity>
+    );
+  };
+  const [showModal, setShowModal] = useState(false);
   return (
-    <View style={{ flex: 1 }}>
-      <Agenda
-        items={{
-          "2022-04-12": [{ name: "item 1 - any js object" }],
-          "2022-04-13": [{ name: "item 2 - any js object", height: 80 }],
-          "2022-04-14": [],
-          "2022-04-15": [
-            { name: "item 3 - any js object" },
-            { name: "any js object" },
-          ],
-        }}
-        theme={{
-          agendaDayTextColor: "yellow",
-          agendaDayNumColor: "green",
-          agendaTodayColor: "red",
-          agendaKnobColor: "blue",
-        }}
-      />
-    </View>
+    <NativeBaseProvider>
+      <View style={{ flex: 1 }}>
+        <Agenda
+          items={items}
+          loadItemsForMonth={loadItems}
+          renderItem={renderItem}
+          hideKnob={true}
+          showClosingKnob={true}
+          theme={{ agendaKnobColor: "#00aeff" }}
+        />
+        <Fab
+          renderInPortal={false}
+          icon={<MaterialCommunityIcons color="white" name="plus" />}
+          onPress={() => setShowModal(true)}
+        />
+        <Modal isOpen={showModal} onClose={() => setShowModal(false)}>
+          <Modal.Content maxWidth="400px">
+            <Modal.CloseButton />
+            <Modal.Header>Contact Us</Modal.Header>
+            <Modal.Body>
+              <FormControl>
+                <FormControl.Label>Name</FormControl.Label>
+                <Input />
+              </FormControl>
+              <FormControl mt="3">
+                <FormControl.Label>Email</FormControl.Label>
+                <Input />
+              </FormControl>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button.Group space={2}>
+                <Button
+                  variant="ghost"
+                  colorScheme="blueGray"
+                  onPress={() => {
+                    setShowModal(false);
+                  }}
+                >
+                  Cancel
+                </Button>
+                <Button
+                  onPress={() => {
+                    setShowModal(false);
+                  }}
+                >
+                  Save
+                </Button>
+              </Button.Group>
+            </Modal.Footer>
+          </Modal.Content>
+        </Modal>
+      </View>
+    </NativeBaseProvider>
   );
 };
 export default Home;
