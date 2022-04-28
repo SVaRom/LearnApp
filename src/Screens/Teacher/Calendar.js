e1942976af4b8257f8db68dfbf3edf32c8a5d948
Sebastián Rodríguez Martínez <30491953+SLegendary@users.noreply.github.com>
2022-04-27 21:44:10 -0500

    Update Calendar y Home

--- src/Screens/Teacher/Calendar.js
+++ src/Screens/Teacher/Calendar.js
@@ -1,39 +1,75 @@
 import React, { useState } from "react";
-import { View } from "react-native";
-import { Calendar } from "react-native-calendars";
-import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import { NativeBaseProvider, View, Box, Text, Pressable } from "native-base";
+import { Agenda } from "react-native-calendars";
 
-const Calendario = ({ navigation, data }) => {
-  return (
-    <View style={{ flex: 1 }}>
-      <Calendar
-        onDayPress={(day) => {
-          console.log("selected day", day);
-        }}
-        hideArrows={false}
-        renderArrow={(direction) =>
-          direction === "left" ? (
-            <MaterialCommunityIcons
-              name="chevron-left"
-              color="#B2BEB5"
-              size={26}
-            />
-          ) : (
-            <MaterialCommunityIcons
-              name="chevron-right"
-              color="#B2BEB5"
-              size={26}
-            />
-          )
-        }
-        onPressArrowLeft={(subtractMonth) => subtractMonth()}
-        onPressArrowRight={(addMonth) => addMonth()}
-        disableArrowLeft={false}
-        disableArrowRight={false}
-        enableSwipeMonths={true}
-      />
-    </View>
-  );
+const timeToString = (time) => {
+  const date = new Date(time);
+  return date.toISOString().split("T")[0];
 };
 
-export default Calendario;
+const hourToString = (timestamp) => {
+  const date = new Date(timestamp * 1000);
+  let hour = date.getHours();
+  let minutes = date.getMinutes();
+  return hour + ":" + minutes;
+};
+
+const Calendar = () => {
+  const [items, setItems] = useState({});
+  const loadItems = (day) => {
+    setTimeout(() => {
+      for (let i = -1; i < 5; i++) {
+        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+        console.log(time);
+        const strTime = timeToString(time);
+        const strClock = hourToString(time);
+        console.log(strTime);
+        console.log(strClock);
+        if (!items[strTime]) {
+          items[strTime] = [];
+          const numItems = Math.floor(Math.random() * 3);
+          for (let j = 0; j < numItems; j++) {
+            items[strTime].push({
+              name: "Asesoria del día " + strTime,
+              height: Math.max(50, Math.floor(Math.random() * 150)),
+              hora: strClock,
+            });
+          }
+        }
+      }
+      const newItems = {};
+      Object.keys(items).forEach((key) => {
+        newItems[key] = items[key];
+      });
+      setItems(newItems);
+    }, 3);
+  };
+
+  const renderItem = (item) => {
+    return (
+      <Pressable style={{ marginRight: 10, marginTop: 17 }}>
+        <Box>
+          <Text>{item.name}</Text>
+          <Text>{item.time}</Text>
+          <Text>{item.hora}</Text>
+        </Box>
+      </Pressable>
+    );
+  };
+
+  return (
+    <NativeBaseProvider>
+      <View style={{ flex: 1 }}>
+        <Agenda
+          items={items}
+          renderItem={renderItem}
+          loadItemsForMonth={loadItems}
+          hideKnob={true}
+          showClosingKnob={true}
+          theme={{ agendaKnobColor: "#00aeff" }}
+        />
+      </View>
+    </NativeBaseProvider>
+  );
+};
+export default Calendar;
--- src/Screens/Teacher/Home.js
+++ src/Screens/Teacher/Home.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { TouchableOpacity } from "react-native";
 import {
   NativeBaseProvider,
   View,
@@ -7,76 +6,157 @@ import {
   Box,
   Modal,
   FormControl,
-  Input,
   Button,
+  Text,
+  Pressable,
+  FlatList,
+  HStack,
+  Avatar,
+  VStack,
+  Spacer,
+  Input,
 } from "native-base";
-import { Agenda } from "react-native-calendars";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
-
-const timeToString = (time) => {
-  const date = new Date(time);
-  return date.toISOString().split("T")[0];
-};
+import DateTimePicker from "@react-native-community/datetimepicker";
+import { Picker } from "@react-native-picker/picker";
 
 const Home = ({ navigation, data }) => {
-  const [items, setItems] = useState({});
-
-  const loadItems = (day) => {
-    setTimeout(() => {
-      for (let i = -15; i < 85; i++) {
-        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
-        const strTime = timeToString(time);
-        if (!items[strTime]) {
-          items[strTime] = [];
-          const numItems = Math.floor(Math.random() * 3 + 1);
-          for (let j = 0; j < numItems; j++) {
-            items[strTime].push({
-              name: "Item for " + strTime + " #" + j,
-              height: Math.max(50, Math.floor(Math.random() * 150)),
-            });
-          }
-        }
-      }
-      const newItems = {};
-      Object.keys(items).forEach((key) => {
-        newItems[key] = items[key];
-      });
-      setItems(newItems);
-    }, 1000);
-  };
-
-  const renderItem = (item) => {
-    return (
-      <TouchableOpacity style={{ marginRight: 10, marginTop: 17 }}>
-        <Box
-          rounded="xl"
-          w="100%"
-          m={1}
-          bg="coolGray.400"
-          p="6"
-          shadow={2}
-          _text={{
-            fontSize: "md",
-            fontWeight: "bold",
-            color: "white",
-          }}
-        >
-          {item.name}
-        </Box>
-      </TouchableOpacity>
-    );
-  };
   const [showModal, setShowModal] = useState(false);
+  const [showModalEdit, setShowModalEdit] = useState(false);
+  const [date, setDate] = useState(new Date());
+  const [mode, setMode] = useState("date");
+  const [show, setShow] = useState(false);
+  const [textT, setTextT] = useState("HH:MM");
+  const [textD, setTextD] = useState("DD/MM/YYYY");
+  const [materia, setMateria] = useState();
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setShow(false);
+    setDate(currentDate);
+    let tempDate = new Date(currentDate);
+    let fDate =
+      tempDate.getDate() +
+      "/" +
+      (tempDate.getMonth() + 1) +
+      "/" +
+      tempDate.getFullYear();
+    let fTime = tempDate.getHours() + ":" + tempDate.getMinutes();
+    setTextD(fDate);
+    setTextT(fTime);
+  };
+
+  const showMode = (currentMode) => {
+    setShow(true);
+    setMode(currentMode);
+  };
+
+  const HandleAdd = () => {
+    console.log(textD);
+    console.log(textT);
+    console.log(materia);
+    setShowModal(false);
+  };
+
+  const HandleEdit = () => {
+    console.log(textD);
+    console.log(textT);
+    console.log(materia);
+    setShowModalEdit(false);
+  };
+
+  const dataItems = [
+    {
+      id: "1",
+      nameMateria: "Calculo Diferencial",
+      timeAsesoria: "12:30",
+      fechaAsesoria: "04/05/2022",
+      avatarUrl: "CD",
+    },
+    {
+      id: "2",
+      nameMateria: "Algebra lineal",
+      timeAsesoria: "14:00",
+      fechaAsesoria: "07/05/2022",
+      avatarUrl: "AL",
+    },
+    {
+      id: "3",
+      nameMateria: "Algebra lineal",
+      timeAsesoria: "10:20",
+      fechaAsesoria: "13/05/2022",
+      avatarUrl: "AL",
+    },
+    {
+      id: "4",
+      nameMateria: "Calculo Diferencial",
+      timeAsesoria: "8:30",
+      fechaAsesoria: "04/05/2022",
+      avatarUrl: "CD",
+    },
+    {
+      id: "5",
+      nameMateria: "Calculo Diferencial",
+      timeAsesoria: "11:20",
+      fechaAsesoria: "05/06/2022",
+      avatarUrl: "CD",
+    },
+  ];
+
   return (
     <NativeBaseProvider>
       <View style={{ flex: 1 }}>
-        <Agenda
-          items={items}
-          loadItemsForMonth={loadItems}
-          renderItem={renderItem}
-          hideKnob={true}
-          showClosingKnob={true}
-          theme={{ agendaKnobColor: "#00aeff" }}
+        <FlatList
+          data={dataItems}
+          renderItem={({ item }) => (
+            <Pressable onPress={() => setShowModalEdit(true)}>
+              <Box
+                borderBottomWidth="1"
+                _dark={{
+                  borderColor: "muted.50",
+                }}
+                borderColor="muted.800"
+                pl="4"
+                pr="5"
+                py="2"
+              >
+                <HStack space={3} justifyContent="space-between">
+                  <Avatar size="48px">{item.avatarUrl}</Avatar>
+                  <VStack>
+                    <Text
+                      _dark={{
+                        color: "warmGray.50",
+                      }}
+                      color="coolGray.800"
+                      bold
+                    >
+                      {item.nameMateria}
+                    </Text>
+                    <Text
+                      color="coolGray.600"
+                      _dark={{
+                        color: "warmGray.200",
+                      }}
+                    >
+                      {item.fechaAsesoria}
+                    </Text>
+                  </VStack>
+                  <Spacer />
+                  <Text
+                    fontSize="xs"
+                    _dark={{
+                      color: "warmGray.50",
+                    }}
+                    color="coolGray.800"
+                    alignSelf="flex-start"
+                  >
+                    {item.timeAsesoria}
+                  </Text>
+                </HStack>
+              </Box>
+            </Pressable>
+          )}
+          keyExtractor={(item) => item.id}
         />
         <Fab
           renderInPortal={false}
@@ -86,16 +166,43 @@ const Home = ({ navigation, data }) => {
         <Modal isOpen={showModal} onClose={() => setShowModal(false)}>
           <Modal.Content maxWidth="400px">
             <Modal.CloseButton />
-            <Modal.Header>Contact Us</Modal.Header>
+            <Modal.Header>Agregar asesoria</Modal.Header>
             <Modal.Body>
               <FormControl>
-                <FormControl.Label>Name</FormControl.Label>
-                <Input />
+                <FormControl.Label>Materia</FormControl.Label>
+                <Picker
+                  selectedValue={materia}
+                  placeholder="Seleccionar Materia"
+                  onValueChange={(itemValue, itemIndex) =>
+                    setMateria(itemValue)
+                  }
+                >
+                  <Picker.Item label="Calculo Diferencial" value="cd" />
+                  <Picker.Item label="Programación Web" value="web" />
+                  <Picker.Item label="Desarrollo movil" value="movdev" />
+                </Picker>
               </FormControl>
-              <FormControl mt="3">
-                <FormControl.Label>Email</FormControl.Label>
-                <Input />
+              <FormControl>
+                <FormControl.Label>Fecha</FormControl.Label>
+                <Pressable onPress={() => showMode("date")}>
+                  <Text fontSize="lg">{textD}</Text>
+                </Pressable>
               </FormControl>
+              <FormControl>
+                <FormControl.Label>Hora</FormControl.Label>
+                <Pressable onPress={() => showMode("time")}>
+                  <Text fontSize="lg">{textT}</Text>
+                </Pressable>
+              </FormControl>
+              {show && (
+                <DateTimePicker
+                  testID="dateTimePicker"
+                  value={date}
+                  mode={mode}
+                  is24Hour={true}
+                  onChange={onChange}
+                />
+              )}
             </Modal.Body>
             <Modal.Footer>
               <Button.Group space={2}>
@@ -106,15 +213,57 @@ const Home = ({ navigation, data }) => {
                     setShowModal(false);
                   }}
                 >
-                  Cancel
+                  Cancelar
                 </Button>
+                <Button onPress={() => HandleAdd()}>Agregar</Button>
+              </Button.Group>
+            </Modal.Footer>
+          </Modal.Content>
+        </Modal>
+
+        <Modal isOpen={showModalEdit} onClose={() => setShowModalEdit(false)}>
+          <Modal.Content maxWidth="400px">
+            <Modal.CloseButton />
+            <Modal.Header>Editar asesoria</Modal.Header>
+            <Modal.Body>
+              <FormControl isDisabled>
+                <FormControl.Label>Materia</FormControl.Label>
+                <Input placeholder={materia}>{materia}</Input>
+              </FormControl>
+              <FormControl>
+                <FormControl.Label>Fecha</FormControl.Label>
+                <Pressable onPress={() => showMode("date")}>
+                  <Text fontSize="lg">{textD}</Text>
+                </Pressable>
+              </FormControl>
+              <FormControl>
+                <FormControl.Label>Hora</FormControl.Label>
+                <Pressable onPress={() => showMode("time")}>
+                  <Text fontSize="lg">{textT}</Text>
+                </Pressable>
+              </FormControl>
+              {show && (
+                <DateTimePicker
+                  testID="dateTimePicker"
+                  value={date}
+                  mode={mode}
+                  is24Hour={true}
+                  onChange={onChange}
+                />
+              )}
+            </Modal.Body>
+            <Modal.Footer>
+              <Button.Group space={2}>
                 <Button
+                  variant="ghost"
+                  colorScheme="blueGray"
                   onPress={() => {
-                    setShowModal(false);
+                    setShowModalEdit(false);
                   }}
                 >
-                  Save
+                  Cancelar
                 </Button>
+                <Button onPress={() => HandleEdit()}>Editar</Button>
               </Button.Group>
             </Modal.Footer>
           </Modal.Content>
