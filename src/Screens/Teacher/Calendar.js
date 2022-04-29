763ca59e05da5e2d58a7cc27458f04e89212ed79
Sebastián Rodríguez Martínez <30491953+SLegendary@users.noreply.github.com>
2022-04-28 19:17:15 -0500

    Calendar update

--- src/Screens/Teacher/Calendar.js
+++ src/Screens/Teacher/Calendar.js
@@ -5,11 +5,11 @@ import { Agenda } from "react-native-calendars";
 const Calendar = ({ navigation, route }) => {
   const [items, setItems] = useState({});
   const fechas = [
-    { date: "2022-04-28", name: "Asesoria Robles", time: "12:00" },
-    { date: "2022-04-28", name: "Asesoria Mascorro", time: "13:00" },
-    { date: "2022-04-28", name: "Asesoria Ilda", time: "14:00" },
-    { date: "2022-04-29", name: "Asesoria Mascorro", time: "13:00" },
-    { date: "2022-04-29", name: "Asesoria Ilda", time: "14:00" },
+    { id: "1", date: "2022-04-28", name: "Asesoria Robles", time: "12:00" },
+    { id: "2", date: "2022-04-28", name: "Asesoria Mascorro", time: "13:00" },
+    { id: "3", date: "2022-04-28", name: "Asesoria Ilda", time: "14:00" },
+    { id: "4", date: "2022-04-29", name: "Asesoria Mascorro", time: "13:00" },
+    { id: "5", date: "2022-04-29", name: "Asesoria Ilda", time: "14:00" },
   ];
   const loadItems = () => {
     for (let i = 0; i < fechas.length; i++) {
@@ -17,8 +17,10 @@ const Calendar = ({ navigation, route }) => {
         items[fechas[i].date] = [];
       }
     }
+    console.log(items);
     for (let i = 0; i < fechas.length; i++) {
       items[fechas[i].date].push(fechas[i]);
+      //console.log(items);
     }
     setItems(items);
   };
@@ -40,11 +42,30 @@ const Calendar = ({ navigation, route }) => {
       <View style={{ flex: 1 }} on>
         <Agenda
           items={items}
-          renderItem={renderItem}
-          loadItemsForMonth={loadItems}
+          renderItem={(item) => {
+            return (
+              <Pressable style={{ marginRight: 10, marginTop: 17 }}>
+                <Box>
+                  <Text>{item.name}</Text>
+                  <Text>{item.time}</Text>
+                </Box>
+              </Pressable>
+            );
+          }}
+          loadItemsForMonth={() => {
+            for (let i = 0; i < fechas.length; i++) {
+              if (!items[fechas[i].date]) {
+                items[fechas[i].date] = [];
+              }
+            }
+            console.log(items);
+            for (let i = 0; i < fechas.length; i++) {
+              items[fechas[i].date].push(fechas[i]);
+              //console.log(items);
+            }
+            setItems(items);
+          }}
           hideKnob={true}
-          showClosingKnob={true}
-          theme={{ agendaKnobColor: "#00aeff" }}
         />
       </View>
     </NativeBaseProvider>
